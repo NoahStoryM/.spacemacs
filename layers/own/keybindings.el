b62e3b16774a91a1863720455761fcb0de8b286b
NoahStoryM <noahstorym@gmail.com>
2025-05-05 23:10:43 +0800

    Add some keybindings

--- layers/own/keybindings.el
+++ layers/own/keybindings.el
@@ -13,3 +13,13 @@
 (keyboard-translate ?\[ ?\()
 (keyboard-translate ?\) ?\])
 (keyboard-translate ?\] ?\))
+
+(define-key evil-emacs-state-map (kbd "<C-tab>") 'evil-normal-state)
+(define-key evil-hybrid-state-map (kbd "<C-tab>") 'evil-normal-state)
+(define-key evil-insert-state-map (kbd "<C-tab>") 'evil-normal-state)
+
+(define-key evil-hybrid-state-map (kbd "C-n") 'nil)
+(define-key evil-hybrid-state-map (kbd "C-p") 'nil)
+(define-key evil-ex-completion-map (kbd "C-b") 'nil)
+(define-key evil-ex-completion-map (kbd "C-f") 'nil)
+(define-key evil-ex-search-keymap (kbd "C-f") 'nil)
