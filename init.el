5c4e2489309d5dd3056c4e913e4f659820180ad5
NoahStoryM <NoahStoryM@gmail.com>
2025-05-29 13:09:10 +0800

    Using `MapleMono` instead of `Hack`

--- init.el
+++ init.el
@@ -218,8 +218,8 @@ It should only modify the values of Spacemacs settings."
    ;; fixed-pitch faces. The `:size' can be specified as
    ;; a non-negative integer (pixel size), or a floating-point (point size).
    ;; Point size is recommended, because it's device independent. (default 10.0)
-   dotspacemacs-default-font '("Hack"
-                               :size 18.0
+   dotspacemacs-default-font '("MapleMono"
+                               :size 18.5
                                :weight normal
                                :width normal
                                :powerline-scale 1.1)
