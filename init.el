01ebdce45130dbb77c58c8ab9dbe1fb8d69183b9
NoahStoryM <NoahStoryM@gmail.com>
2025-05-29 19:12:50 +0800

    Set Chinese font

--- init.el
+++ init.el
@@ -586,6 +586,7 @@ This function is called at the very end of Spacemacs startup, after layer
 configuration.
 Put your configuration code here, except for variables that should be set
 before packages are loaded."
+  (spacemacs//set-monospaced-font "Maple Mono NL" "Maple Mono NL CN" 25 25)
   )
 
 ;; Do not write anything past this comment. This is where Emacs will
