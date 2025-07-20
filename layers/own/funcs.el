a1a3bbffe40557ce9286695527ce8aab92dc5287
NoahStoryM <NoahStoryM@gmail.com>
2025-07-20 17:53:22 +0800

    [spacemacs-light-theme] Update `cblk-bg`

--- layers/own/funcs.el
+++ layers/own/funcs.el
@@ -21,7 +21,7 @@
      (base          . ,(if (eq theme 'spacemacs-dark) (if (true-color-p) "#bbc2cf" "#bbc2cf") (if (true-color-p) "#000000" "#000000")))
      (bg1           . ,(if (eq theme 'spacemacs-dark) (if (true-color-p) "#282a36" "#29282e") (if (true-color-p) "#fdf7e3" "#ffffff")))
      (bg2           . ,(if (eq theme 'spacemacs-dark) (if (true-color-p) "#23212a" "#1c1c1c") (if (true-color-p) "#eeede8" "#e4e4e4")))
-     (cblk-bg       . ,(if (eq theme 'spacemacs-dark) (if (true-color-p) "#2f2b33" "#262626") (if (true-color-p) "#fbf8ef" "#ffffff")))
+     (cblk-bg       . ,(if (eq theme 'spacemacs-dark) (if (true-color-p) "#2f2b33" "#262626") (if (true-color-p) "#fff8dc" "#ffffff")))
      (func          . ,(if (eq theme 'spacemacs-dark) (if (true-color-p) "#bc6ec5" "#d75fd7") (if (true-color-p) "#705091" "#8700af")))
      (comment       . ,(if (eq theme 'spacemacs-dark) (if (true-color-p) "#2ca6b3" "#008787") (if (true-color-p) "#deb887" "#008787")))
      (comment-light . ,(if (eq theme 'spacemacs-dark) (if (true-color-p) "#2ca6b3" "#008787") (if (true-color-p) "#a49da5" "#008787")))
