62cd224ad553d38a5912130065afb6e29d5b86c8
NoahStoryM <NoahStoryM@gmail.com>
2025-08-12 11:53:53 +0800

    Set chat-width-table

--- layers/own/config.el
+++ layers/own/config.el
@@ -16,6 +16,9 @@
   (global-font-lock-mode t)
   (global-prettify-symbols-mode t))
 
+(progn ;; Char width
+  (set-char-table-range char-width-table '(#x2200 . #x22FF) 1))
+
 (progn ;; Electric pair
   (custom-set-variables
    '(electric-pair-delete-adjacent-pairs nil)
